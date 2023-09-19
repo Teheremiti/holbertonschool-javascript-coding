de2dca3c903d1ec4a453ccd9b9b6bf0f4c2dadb5
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-09-19 15:22:42 +0200

    Parse the response to JSON

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,9 +3,10 @@ const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 const request = require('request');
 
-request(url, (err, movie) => {
+request(url, (err, response, body) => {
   if (err) {
     console.error(err);
   }
+  const movie = JSON.parse(body);
   console.log(movie.title);
 });
