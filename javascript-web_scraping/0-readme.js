4d1da04318ff8b70f9bfb53999878e4080a26bd3
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-09-19 14:57:36 +0200

    Read and print

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+import require from fs
+
+const filePath = process.argv[2];
+const fs = require('fs');
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(data);
+})
