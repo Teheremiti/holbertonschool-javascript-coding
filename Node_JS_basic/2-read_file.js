18ef75d337f7eebb49668100f5eac98724f657e9
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-10-06 15:18:15 +0200

    Read a file synchronously

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,27 @@
+module.exports = function counStudents(path) {
+  const fs = require('fs');
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+    const lines = data.toString().split('\n');
+    console.log(`Number of students: ${lines.length - 2}`);
+
+    let CSstudents = '';
+    let CScount = 0;
+    let SWEstudents = '';
+    let SWEcount = 0;
+    for (const line of lines.slice(1, -1)) {
+      if (line.endsWith('CS')) {
+        CScount++;
+        CSstudents += line.split(',')[0] + ', ';
+      } else if (line.endsWith('SWE')) {
+        SWEcount++;
+        SWEstudents += line.split(',')[0] + ', ';
+      }
+    }
+    console.log(`Number of students in CS: ${CScount}. List: ${CSstudents.slice(0, -2)}`);
+    console.log(`Number of students in SWE: ${SWEcount}. List: ${SWEstudents.slice(0, -2)}`);
+  });
+}
