0ddf0f9b3e7874135882a284e47a6c3d8573cad7
Teheremiti <120781178+Teheremiti@users.noreply.github.com>
2023-10-08 16:57:15 +0200

    Use try/catch

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,29 +1,30 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    }
-    const lines = data.toString().split('\n').filter((line) => line.trim() !== '');
-    console.log(`Number of students: ${lines.length - 1}`);
+  let data = '';
+  try {
+    data = fs.readFileSync(path, 'utf-8');
+  } catch {
+    throw new Error('Cannot load the database');
+  }
+  const lines = data.toString().split('\n').filter((line) => line.trim() !== '');
+  console.log(`Number of students: ${lines.length - 1}`);
 
-    let CSstudents = '';
-    let CScount = 0;
-    let SWEstudents = '';
-    let SWEcount = 0;
-    for (const line of lines.slice(1)) {
-      if (line.endsWith('CS')) {
-        CScount += 1;
-        CSstudents += `${line.split(',')[0]}, `;
-      } else if (line.endsWith('SWE')) {
-        SWEcount += 1;
-        SWEstudents += `${line.split(',')[0]}, `;
-      }
+  let CSstudents = '';
+  let CScount = 0;
+  let SWEstudents = '';
+  let SWEcount = 0;
+  for (const line of lines.slice(1)) {
+    if (line.endsWith('CS')) {
+      CScount += 1;
+      CSstudents += `${line.split(',')[0]}, `;
+    } else if (line.endsWith('SWE')) {
+      SWEcount += 1;
+      SWEstudents += `${line.split(',')[0]}, `;
     }
-    console.log(`Number of students in CS: ${CScount}. List: ${CSstudents.slice(0, -2)}`);
-    console.log(`Number of students in SWE: ${SWEcount}. List: ${SWEstudents.slice(0, -2)}`);
-  });
-};
+  }
+  console.log(`Number of students in CS: ${CScount}. List: ${CSstudents.slice(0, -2)}`);
+  console.log(`Number of students in SWE: ${SWEcount}. List: ${SWEstudents.slice(0, -2)}`);
+}
 
 module.exports = countStudents;
